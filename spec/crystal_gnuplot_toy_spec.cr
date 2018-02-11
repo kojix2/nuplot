69d919175f83d6217b5750c50b7df4568792fcb1
kojix2 <2xijok@gmail.com>
2018-02-11 22:18:41 +0900

    1d array and 2d array

--- spec/crystal_gnuplot_toy_spec.cr
+++ spec/crystal_gnuplot_toy_spec.cr
@@ -2,4 +2,5 @@ require "./spec_helper"
 
 describe Array do
   [1,2,3].to_gp.should eq "1\n2\n3\ne"
+  [[1,2,3],[4,5,6]].to_gp.should eq "1 4\n2 5\n3 6\ne"
 end
--- src/crystal_gnuplot_toy/array.cr
+++ src/crystal_gnuplot_toy/array.cr
@@ -1,15 +1,31 @@
 class Array
+
+  # To Gnuplot Data
+  # I know this code is not cool at all.
+  # I want to know if there is a better way. 
   def to_gp
     case self
-    when Array(Array(Int8 | Int16 | Int32 | Int64 | UInt8 | UInt32 | UInt64 | Float32 | Float64))
+    when Array(Array(Float32))
       ar = self.transpose
-      ar3 = ar.map do |ar2|
-        ar2.join(" ")
-      end
-      ar3.join("\n") + "\ne"
+      mapjoin(ar)
+    when Array(Array(Float64))
+      ar = self.transpose
+      mapjoin(ar)
+    when Array(Array(Int32))
+      ar = self.transpose
+      mapjoin(ar)
+    when Array(Array(Int64))
+      ar = self.transpose
+      mapjoin(ar)
     else
-      str = self.join("\n")
-      str += "\ne"
+      str = self.join("\n") + "\ne"
     end
+  end
+
+  private def mapjoin(ar)
+    ar3 = ar.map do |ar2|
+      ar2.join(" ")
+    end
+    ar3.join("\n") + "\ne"
   end
 end
