b521b8a64348d2dab00cc3db2947a155af73d354
kojix2 <2xijok@gmail.com>
2018-02-11 21:09:44 +0900

    third commit

---
+++ examples/lines.cr
@@ -0,0 +1,21 @@
+require "../src/crystal_gnuplot_toy.cr"
+
+memo_x1 = 0
+memo_x2 = 0
+x1 = Array(Float64).new(100,0.0)
+x2 = Array(Float64).new(100,0.0)
+
+100.times do |i|
+  memo_x1 = memo_x1 + rand(-0.9..1.0)
+  x1[i] = memo_x1
+  memo_x2 = memo_x2 + rand(-0.9..1.0)
+  x2[i] = memo_x2
+end
+
+
+Gnuplot.plot do |s|
+  s << "set title 'LINES'"
+  s << "plot '-' with lines title 'x1', '-' with lines title 'x2'"
+  s << x1.to_gp
+  s << x2.to_gp
+end
--- src/crystal_gnuplot_toy/array.cr
+++ src/crystal_gnuplot_toy/array.cr
@@ -1,7 +1,7 @@
 class Array
   def to_gp
-    case self.first 
-    when Array
+    case self
+    when Array(Array(Int8 | Int16 | Int32 | Int64 | UInt8 | UInt32 | UInt64 | Float32 | Float64))
       ar = self.transpose
       ar3 = ar.map do |ar2|
         ar2.join(" ")
